f731621109fa31a12a2b469c4469733e932d2864
Jia En <tujiaen@gmail.com>
2024-09-18 19:15:26 -0400

    scaffold endpoints

--- src/main.py
+++ src/main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
+@app.post("/conversations")
+async def create_conversation():
+    return {"message": "Hello World"}
+
+@app.get("/conversations")
+async def list_conversations():
+    return {"message": "Hello World"}
+
+@app.put("/conversations/{id}")
+async def update_conversation(id: int):
+    return {"message": "Hello World"}
+
+@app.get("/conversations/{id}")
+async def read_conversation(id: int):
+    return {"message": "Hello World"}
+
+@app.delete("/conversations/{id}")
+async def delete_conversation(id: int):
+    return {"message": "Hello World"}
+
+@app.post("/queries")
+async def create_query():
     return {"message": "Hello World"}
